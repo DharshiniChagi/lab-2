075f4cc0e92e555cd24c562f497e63d2a38e1cbb
DharshiniChagi <chagidharishini@gmail.com>
2024-02-08 12:48:17 +0530

    Geometrical calculations

---
+++ area.java
@@ -0,0 +1,15 @@
+ class area{
+   
+    double area_of_triangle(float op_1, float op_2){
+        return(0.5*op_1*op_2);
+
+    }
+    // The `area_of_rectangle` method is calculating the area of a rectangle. It takes two parameters
+    // `op_1` and `op_2`, which represent the length and width of the rectangle respectively. The
+    // method multiplies the length and width together and returns the result as the area of the
+    // rectangle.
+    double area_of_rectangle(float op_1, float op_2){
+        return(op_1*op_2);
+
+    }
+}
